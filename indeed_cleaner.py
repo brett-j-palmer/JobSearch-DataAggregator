b290f70f0737c5e81c8d08fc25a7031b9db39d32
Brett Palmer <brett.palmer@maine.edu>
2024-12-08 21:23:11 -0500

    fixed bug for salaries containing decimals

--- indeed_cleaner.py
+++ indeed_cleaner.py
@@ -2,8 +2,6 @@
 # Brett Palmer and Monica Agneta
 
 import pandas as pd
-import re 
-import  math
 
 df = pd.read_csv('indeed_jobs.csv')
 # df = df.drop(df.columns[0], axis=1)
@@ -11,24 +9,25 @@ df = pd.read_csv('indeed_jobs.csv')
 for index in df.index:
     salary = df["salary"][index]
     if isinstance(salary, str):
-        print(salary, end="  ")
+        # print(salary, end="  ")
         salary = salary.replace(",","")
         if "-" in salary:
             salary_range = salary.split("-")
-            lower_salary = int(re.sub("\D", "", salary_range[0]))
-            upper_salary = int(re.sub("\D", "", salary_range[1]))
+            lower_salary = float("".join([char for char in salary_range[0] if (char.isdigit() or char==".")] ))
+            upper_salary = float("".join([char for char in salary_range[1] if (char.isdigit() or char==".")] ))
             avg_salary = (lower_salary + upper_salary)/2
         else:
-            avg_salary = int(re.sub("\D", "", salary))
+            avg_salary = float("".join([char for char in salary if (char.isdigit() or char==".")] ))
         if "a year" in salary:
             yearly_salary = avg_salary
         elif "an hour" in salary:
             yearly_salary = avg_salary * 40 * 52
         else:
             yearly_salary = None
+        yearly_salary = round(yearly_salary,2)
         df.at[index, 'salary'] = yearly_salary
-        print(yearly_salary)
+        # print(yearly_salary)
 
 df2 = df.drop_duplicates()
 
-df2.to_csv('indeed_jobs_cleaned.csv')
+df2.to_csv('indeed_jobs_cleaned.csv', index=False)
